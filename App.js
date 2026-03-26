5411f9b3c2f25981d2e320785d7f877a815cba96
PAULA MARTINS DE SOUZA <paula.msouza6@senacsp.edu.br>
2026-03-25 21:40:36 -0300

    aula 25/03

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.caixaTopo}>
+        <Text style={styles.textao}>Header do BCC</Text>
+        <Text style={styles.textinho}>Bem vindo a Mobile</Text>
+      </View>
+      <View style={styles.caixaMeio}> 
+        <Text style={styles.textao}>Lokinho</Text>
+      </View>
+      <View style={styles.caixaTopo}>
+        <Text style={styles.textinho}>MATHEUS OTARIO</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textinho: {
+    fontSize: '20px',
+    fontWeight: '1000',
+  },
+  caixaTopo: {
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  caixaMeio: {
+    fontSize: '30px',
+    height: 500,
+    width: '100%',
+    backgroundColor: 'pink', 
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textao: {
+    fontSize: '50px',
+    fontWeight: '100',
+  }
 });
